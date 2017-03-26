22dc4f4bffeccdbb4ff18a9b75b463342c3673e7
Anton Shulzhenko <shulzhenkomailbox@gmail.com>
2017-03-27 00:30:04 +0300

    sliced old version

--- src/js/common/header-contacts.js
+++ src/js/common/header-contacts.js
@@ -13,8 +13,6 @@
   var timingFunction    = 'Power1.easeInOut';
   var winWidth          = window.innerWidth;
 
-  if (!mobileContainer.length || !headerContacts.length) return;
-
   function setMobileAddress() {
     var text = location.querySelector('.contacts__text').innerHTML;
     var href = 'http://maps.google.com?q=' + text;
